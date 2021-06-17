fde1613609d982bf83e54c3fbe0aac48951c5f5f
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-16 19:49:33 -0500

    constructor method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
+        Doctor myDoctor = new Doctor("Lorena Jiménez Diaz");
         myDoctor.name = "Gerardo Barrera Moreno";
         myDoctor.showName();
     }
