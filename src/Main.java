948bfe5cccefb78ab1f516d7539a116d477096a8
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-17 07:27:55 -0500

    Constructor overloading

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,10 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
+        // showMenu();
 
-
-        showMenu();
+        Doctor myDoctor = new Doctor("Lorena Jimenez Diaz", "Medico internista");
+        System.out.println(myDoctor.name);
+        System.out.println(myDoctor.speciality);
     }
 }
