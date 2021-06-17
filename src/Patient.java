c6341c15a025f813183f4c6f31f01f6e70f3a089
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-17 05:59:47 -0500

    challenge 1

---
+++ src/Patient.java
@@ -0,0 +1,79 @@
+public class Patient {
+    public String name;
+    public String email;
+    public String address;
+    public String phoneNumber;
+    public String birthday;
+    public Double weight;
+    public Double height;
+    public String blood;
+
+    Patient (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double wieght) {
+        this.weight = weight;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public String getBlood() {
+        return blood;
+    }
+
+    public void setBlood(String blood) {
+        this.blood = blood;
+    }
+}
