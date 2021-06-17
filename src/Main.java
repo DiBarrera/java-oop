715191540e126d656eca2e6f498cc1f35e18294b
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-17 05:21:57 -0500

    Usage of Final

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,7 @@ import static ui.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
 
-        Doctor myDoctor = new Doctor();
-        myDoctor.name = "Gerardo Barrera Moreno";
-        myDoctor.showName();
-        myDoctor.showId();
 
-        Doctor myDoctorAnn = new Doctor();
-        myDoctor.showId();
 
         showMenu();
     }
