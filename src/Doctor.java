74c3e0cb937006c02e744b9b95af96a4b1ad854c
Diego Barrera Jiménez <di_barrera@hotmail.com>
2021-06-17 20:27:00 -0500

    Polymorphism and overriding the toString method

--- src/Doctor.java
+++ src/Doctor.java
@@ -28,6 +28,15 @@ public class Doctor extends User {
         return availableAppointments;
     }
 
+    @Override
+    public String toString() {
+        return super.toString()
+                + "\nSpeciality: "
+                + speciality
+                + "\nAvailabe: "
+                + availableAppointments.toString();
+    }
+
     public static class AvailableAppointment {
         private int id;
         private Date date;
@@ -60,6 +69,14 @@ public class Doctor extends User {
 
         public void setTime(String time) {
             this.time = time;
+        }
+
+        @Override
+        public String toString() {
+            return "Available Appointments \nDate: "
+                    + date
+                    + "\nTime: "
+                    + time;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,24 @@ public class Main {
 
         // showMenu();
 
-        Doctor myDoctor = new Doctor("Lorena Jimenez Diaz", "Medico internista");
+        Doctor myDoctor = new Doctor("Lorena Jimenez Diaz", "lorena@mail.com");
         myDoctor.addAvailableAppointment(new Date(), "4pm");
         myDoctor.addAvailableAppointment(new Date(), "10am");
         myDoctor.addAvailableAppointment(new Date(), "1pm");
 
+        System.out.println(myDoctor);
+
+/*
         System.out.println(myDoctor.getAvailableAppointments());
 
         for (Doctor.AvailableAppointment availableAppointment: myDoctor.getAvailableAppointments()) {
             System.out.println(availableAppointment.getDate() + " " + availableAppointment.getTime());
         }
+ */
 
         System.out.println();
         System.out.println();
         Patient patient = new Patient("Alejandra", "alejandra@gmail.com");
-
         System.out.println(patient);
     }
 }
